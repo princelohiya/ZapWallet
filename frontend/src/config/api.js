3536c0f9637cafb8db7123f5a9a372b8ae785a8d
Prince lohia <princelohia444@gmail.com>
2026-01-02 16:35:21 +0530

    set to prod url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
 import.meta.env;
 const production = "https://zapwallet.onrender.com";
 const development = "http://localhost:3000";
-export const API_BASE_URL = development;
+export const API_BASE_URL = production;

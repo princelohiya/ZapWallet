869cf0b0b8c0e2cb5730be6ee8723b106e489d8f
Prince lohia <princelohia444@gmail.com>
2026-01-06 15:35:59 +0530

    Fix loader color

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -3,7 +3,7 @@ export const Spinner = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-orange-600"
+        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-purple-500"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
